4a4083b73079c54a83532797ca547618f271b819
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-07 18:11:39 +0530

    new

---
+++ mongo.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+let MONGODB_URL =
+  "mongodb+srv://Shashidhar_5:Shashi%40123@cluster0.kms6f.mongodb.net/OnlineShopping?retryWrites=true";
+let MONGODB_NAME = "OnlineShopping";
+
+let client = new MongoClient(MONGODB_URL);
+let mongo = {
+  db: null,
+  products: null,
+  async connect() {
+    try {
+      await client.connect();
+      this.db = client.db(MONGODB_NAME);
+      this.products = this.db.collection("products");
+      console.log("Selected Database:-", MONGODB_NAME);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
+
+module.exports = mongo;
